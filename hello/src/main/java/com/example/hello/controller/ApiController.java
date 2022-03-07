912f26ccdcb3b25cf93cf6d674d25628a2fd0c6a
JSH <qkdwn48@naver.com>
2022-03-07 23:57:05 +0900

    [4강] JsonInclude, 타입추론 var, Response 내려주기
    1. @JsonInclude(JsonInclude.Include.NON_NULL)
     - Response 에 내려줄 값 확인(null인 값은 내리지 않는다)
    
    2. 타입추론 var
     - `User` user > `var` user = new User();
     - 초기화 값이 있는 지역변수로만 선언 가능
     - 변수명에 집중 가능
     |Map<String, Integer> map = new HashMap<>();
     |var map = new HashMap<String, Integer>();
     - 런타임 오버헤드 X
     |var i = 3으로 지정 시 i는 int로 지정 됨.
     |i = "String" 불가능

--- hello/src/main/java/com/example/hello/controller/ApiController.java
+++ hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -1,8 +1,9 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // 해당 Class 는 Rest API 처리하는 Controller
 @RequestMapping("/api") // RequestMapping URL 을 지정해주는 Annotation
@@ -12,5 +13,21 @@ public class ApiController {
     public String hello(){
 
         return "<div style='background-color:green; width:200px; height:200px;'>a</div>";
+    }
+
+    @GetMapping("/text")
+    public String text(@RequestParam String account){
+        return account;
+    }
+
+    @PostMapping("/json")
+    public User  json(@RequestBody User user){
+        System.out.println(user);
+        return user;
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<User> pub(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
