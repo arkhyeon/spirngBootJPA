a4ff983aac17df5924691a0c3fa9cf9a08baa93b
JSH <qkdwn48@naver.com>
2022-03-09 22:40:24 +0900

    [6강] Ioc, DI
    1. Ioc
     - 스프링에서 일반적인 자바 객체를 new 로 생성하여 개발자가 관리하는 것이 아닌 Spring Container에 모두 맡긴다.
     - 즉 개발자에서 프레임워크로 객체 관리의 권한이 넘어감(제어의 역전)
    
    2. DI(Dependency Injection)
     - 스프링이 알아서 객체의 생명주기 관리
     - 객체를 관리하기위해 주입 받음
    
    3. 장점
     - 의존성으로 부터 격리시켜 코드 테스트 용이
     - DI 통해 불가능한 상황을 Mock와 같은 기술로 안정적 테스트
     - 코드를 확장, 변경 시 영향 최소화(추상화)
     - 순환참조를 막음

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -8,6 +8,18 @@ public class HelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+        System.out.println("main");
+        String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+        // Base 64 encoding
+        Encoder encoder = new Encoder(new Base64Encoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
+
+        // url encoding
+        Encoder urlEncoder = new Encoder(new UrlEncoder());
+        String urlResult = urlEncoder.encode(url);
+        System.out.println(urlResult);
     }
 
 }
---
+++ hello/src/main/java/com/example/hello/IEncoder.java
@@ -0,0 +1,5 @@
+package com.example.hello;
+
+public interface IEncoder {
+    String encode(String message);
+}
