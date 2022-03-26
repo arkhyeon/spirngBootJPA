d7439d30534167de2ad5e85e7bea804bf164afda
JSH <qkdwn48@naver.com>
2022-03-26 23:57:47 +0900

    [19강] RestTemplate 사용
     - Client / Server 통신
     - 스프링에서 제공하는 http 통신에 유용하게 쓸 수 있는 템플릿
    
     1. UriComponentsBuilder
                    .fromUriString("http://localhost:9090")
                    .path("/api/server/hello")
                    .encode()
                    .queryParam("name", "aaaa")
                    .queryParam("age", 99)
                    .build()
                    .toUri();
    
    2. ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
    
     - 반환 타입 result = restTemplate.반환 타입
    
     getForObject / GET /
     주어진 URL 주소로 HTTP GET 메서드로 객체로 결과를 반환받는다
    
     getForEntity / GET
     주어진 URL 주소로 HTTP GET 메서드로 결과는 ResponseEntity로 반환받는다
    
     postForLocation / POST
     POST 요청을 보내고 결과로 헤더에 저장된 URI를 결과로 반환받는다
    
     postForObject / POST
     POST 요청을 보내고 객체로 결과를 반환받는다
    
     postForEntity / POST
     POST 요청을 보내고 결과로 ResponseEntity로 반환받는다
    
     delete / DELETE
     주어진 URL 주소로 HTTP DELETE 메서드를 실행한다
    
     headForHeaders / HEADER
     헤더의 모든 정보를 얻을 수 있으면 HTTP HEAD 메서드를 사용한다
    
     put / PUT
     주어진 URL 주소로 HTTP PUT 메서드를 실행한다
    
     patchForObject / PATCH
     주어진 URL 주소로 HTTP PATCH 메서드를 실행한다
    
     optionsForAllow / OPTIONS
     주어진 URL 주소에서 지원하는 HTTP 메서드를 조회한다
    
     exchange / any
     HTTP 헤더를 새로 만들 수 있고 어떤 HTTP 메서드도 사용가능하다
    
     execute / any
     Request/Response 콜백을 수정할 수 있다

---
+++ client/src/main/java/com/example/client/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.example.client.controller;
+
+import com.example.client.dto.UserResponse;
+import com.example.client.service.RestTemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ApiController {
+
+    private final RestTemplateService restTemlateService;
+
+    public ApiController(RestTemplateService restTemlateService) {
+        this.restTemlateService = restTemlateService;
+    }
+
+    @GetMapping("/hello")
+    public UserResponse getHello(){
+        return restTemlateService.hello();
+    }
+}
