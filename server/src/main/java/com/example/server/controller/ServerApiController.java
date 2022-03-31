887ddf6c80c081c18f2bc3d97ea9554fc7695665
JSH <qkdwn48@naver.com>
2022-03-31 23:56:14 +0900

    [복습] Naver Open Api 연동
    1. UriComponentsBuilder
     - Controller에서 addAttribute로 각각 속성을 지정해주지 않아도 이 class를 이용하면 쉽게 파라미터들 전달
     - Spring 3부터 지원, REST API 호출이후 응답을 받을 때까지 기다리는 동기 방식
    2. fromUriString
     - 주어진 URI 문자열로 초기화되는 빌더를 작성하십시오.
    3. path
     - 빌더의 경로에 추가합니다.
    4. queryParam
     - 쿼리파라미터 키밸류
    5. encode
     - 자동으로 UTF-8로 인코딩 해준다.
    6. build
     - 인스턴스 빌드
    7. toUri
     - 인스턴스에서 URI를 만듭니다
     - Uniform Resource Identifier
     - 인터넷에 있는 자원을 나타내는 유일한 주소

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,12 +4,52 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/server")
 @Slf4j
 public class ServerApiController {
+
+    @GetMapping("/naver")
+    public String naver(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query","갈비집")
+                .queryParam("display", 10)
+                .queryParam("sort", "random")
+//                .encode(Charset.forName("UTF-8"))
+                .encode()
+                .build()
+                .toUri();
+
+        log.info("uri : {}",uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","kiksCFo1tDAdlV9PmQgs")
+                .header("X-Naver-Client-Secret","wBLeZmgyI6")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+
+
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age){
